be0f675040a7ab0fc5e75cbfa9d501352d841331
MaksimToll <tolstik.maksim@yandex.ua>
2015-02-21 17:09:17 +0200

    add some changes

---
+++ src/main/java/com/company/ReadCoordinats.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import com.company.entity.Coordinates;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * This class read file of coordinates, parse , and put there  in ArrayList
+ */
+public class ReadCoordinats {
+    private ArrayList<Coordinates> coordinatesList = new ArrayList<Coordinates>();
+//    private String file ="src/route1.txt";
+
+    public ArrayList<Coordinates> read(String file) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(file)));
+            String s;
+
+            while ((s = bufferedReader.readLine())!=null){
+                coordinatesList.add(tuCoordinates(s));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  coordinatesList;
+
+    }
+    /**
+     * parse coordinate
+     * */
+    private  Coordinates tuCoordinates(String coordinate){
+        String[] lat = coordinate.split(";");
+        Coordinates coordinates = new Coordinates(lat[0], lat[1], lat[2]);
+       // System.out.println(coordinates);
+        return coordinates;
+    }
+}
