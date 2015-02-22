9103c8817fd22fa26ef983f0d6c2fca4d7ecbd16
DrBAXA <drbaxa@hotmail.com>
2015-02-22 17:35:08 +0200

    emulator with controller work

--- map/map.js
+++ map/map.js
@@ -7,7 +7,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
 function initialize() {
 	var defaultMapOptions = {
 		center: {lat : 48.9501, lng : 24.701},
-		zoom: 14
+		zoom: 12
 	};
 	map = new google.maps.Map(document.getElementById('map_container'), defaultMapOptions);
 	getReq();
@@ -23,9 +23,12 @@ function initialize() {
 function addMarker(position){
 	var	marker = new google.maps.Marker({
 		position: position,
-		title: position.timestamp+"",
+		title: position.lat + "; "+position.lng,
 		map: map
 	});
+	if(position.isStation == "true"){
+		marker.setIcon("622.png")
+	}
 	marker.setMap(map);
 }
 
@@ -33,7 +36,7 @@ function addMarker(position){
 //Sending request to server for ATMs
 function getReq() {
     $.ajax({
-        url: "with time.xml",
+        url: "route23bygoogle.xml",
         type: "GET",
         context: document.body,
         dataType: "xml",
@@ -52,7 +55,7 @@ function parse(data) {
 		};
 		point.lat = jQuery(this).attr("latitude")*1;
 		point.lng = jQuery(this).attr("longitude")*1;
-		point.timestamp = jQuery(this).attr("timestamp")*1;
+		point.isStation = jQuery(this).attr("isStation");
 		addMarker(point)
 	})
 	
