4c45935bd8a6ec33165ad4dc4c8171a481a96afb
DrBAXA <drbaxa@hotmail.com>
2015-04-18 15:14:30 +0300

    WebSocket added

--- src/main/java/com/findbus/websocket/WebSocketHandler.java
+++ src/main/java/com/findbus/websocket/WebSocketHandler.java
@@ -1,16 +1,46 @@
 package com.findbus.websocket;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
+@ServerEndpoint("/websocket")
+public class WebSocketHandler {
 
-public class WebSocketHandler extends TextWebSocketHandler {
+    @OnMessage
+    public void onMessage(String message, Session session)
+            throws IOException, InterruptedException {
 
-    @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) {
-        // ...
+        // Print the client message for testing purposes
+        System.out.println("Received: " + message);
+
+        // Send the first message to the client
+        session.getBasicRemote().sendText("This is the first server message");
+
+        // Send 3 messages to the client every 5 seconds
+        int sentMessages = 0;
+        while(sentMessages < 3){
+            Thread.sleep(5000);
+            session.getBasicRemote().
+                    sendText("This is an intermediate server message. Count: "
+                            + sentMessages);
+            sentMessages++;
+        }
+
+        // Send a final message to the client
+        session.getBasicRemote().sendText("This is the last server message");
     }
 
+    @OnOpen
+    public void onOpen() {
+        System.out.println("Client connected");
+    }
 
-
+    @OnClose
+    public void onClose() {
+        System.out.println("Connection closed");
+    }
 }
+
